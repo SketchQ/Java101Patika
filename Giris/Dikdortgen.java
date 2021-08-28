5a33921787a6468ad60610bc698e2427d49d6ece
Erim Serdönmez <eserdnmez@gmail.com>
2021-08-28 23:14:42 +0300

    3_2 Lesson Done

---
+++ Giris/Dikdortgen.java
@@ -0,0 +1,11 @@
+package Giris;
+
+public class Dikdortgen {
+    public static void main(String[] args) {
+        int kisaKenar = 10, uzunKenar = 20;
+        int alan = kisaKenar * uzunKenar;
+        int cevre = 2 * (kisaKenar + uzunKenar);
+        System.out.println(alan);
+        System.out.println(cevre);
+    }
+}
