9d93f413774902380a30235447bf706f61f0d208
Erim Serdönmez <eserdnmez@gmail.com>
2021-08-29 00:54:20 +0300

    3_5 Lesson Done

---
+++ Giris/String.java
@@ -0,0 +1,9 @@
+package Giris;
+
+public class String {
+    public static void main(java.lang.String[] args) {
+        java.lang.String words = "Hello World!";
+        System.out.println(words);
+    }
+    
+}
