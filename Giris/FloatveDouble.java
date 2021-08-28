ecfc4dee6da6d1424d896e6f21501a13bc130668
Erim Serdönmez <eserdnmez@gmail.com>
2021-08-28 23:29:55 +0300

    3_3 Lesson Done

---
+++ Giris/FloatveDouble.java
@@ -0,0 +1,13 @@
+package Giris;
+
+public class FloatveDouble {
+    public static void main(String[] args) {
+        float number1 = 3.14F;
+        System.out.println(number1);
+        float number2 = 3.14f;
+        System.out.println(number2);
+
+        double number3 = 3.14;
+        System.out.println(number3);
+    }
+}
