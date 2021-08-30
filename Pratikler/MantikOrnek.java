b9930ef99c7089b279d5c20669397c9099b6488b
Erim Serdönmez <eserdnmez@gmail.com>
2021-08-30 21:19:39 +0300

    4_2 Lesson Done

---
+++ Pratikler/MantikOrnek.java
@@ -0,0 +1,12 @@
+package Pratikler;
+
+public class MantikOrnek {
+    public static void main(String[] args) {
+        int a = 10, b;
+        b = (a == 1) ? 20 : 30;
+        System.out.println("Value of b is : " + b);
+
+        b = (a == 10) ? 20 : 30;
+        System.out.println("Value of b is : " + b);
+    }
+}
