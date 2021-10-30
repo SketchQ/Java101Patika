62badb0a86824a272c0117b41baf143020d45943
Erim Serdönmez <eserdnmez@gmail.com>
2021-10-30 22:30:38 +0300

    Objects Created

--- Patika Java 101 Ders Notları/8_ Sınıflar - 6 Ders/OOP/src/App.java
+++ Patika Java 101 Ders Notları/8_ Sınıflar - 6 Ders/OOP/src/App.java
@@ -2,6 +2,19 @@ package src;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Car audi = new Car();
+        audi.speed = 10;
+        System.out.println("Audi Hızı : " + audi.speed);
+
+        System.out.println("-------");
+        Car bmw = new Car();
+        bmw.speed = 25;
+        System.out.println("BMW Hızı : " + bmw.speed);
+
+        System.out.println("-------");
+        Car mercedes = new Car();
+        mercedes.speed = 30;
+        System.out.println("Mercedes Hızı : " + mercedes.speed);
+        
     }
 }
