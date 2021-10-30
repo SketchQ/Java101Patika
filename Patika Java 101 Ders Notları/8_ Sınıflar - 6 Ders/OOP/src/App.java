38267c772b8bd9bd437d2d3adfdff4bb8b45c72d
Erim Serdönmez <eserdnmez@gmail.com>
2021-10-30 22:37:58 +0300

    Constructure Done

--- Patika Java 101 Ders Notları/8_ Sınıflar - 6 Ders/OOP/src/App.java
+++ Patika Java 101 Ders Notları/8_ Sınıflar - 6 Ders/OOP/src/App.java
@@ -5,16 +5,17 @@ public class App {
         Car audi = new Car();
         audi.speed = 10;
         System.out.println("Audi Hızı : " + audi.speed);
-
-        System.out.println("-------");
+        Car r8 = new Car("Sports", "Audi", "Red");
+        r8.printInfo();
         Car bmw = new Car();
         bmw.speed = 25;
         System.out.println("BMW Hızı : " + bmw.speed);
-
-        System.out.println("-------");
+        Car m3 = new Car("Sports","BMW","Blue");
+        m3.printInfo();
         Car mercedes = new Car();
         mercedes.speed = 30;
         System.out.println("Mercedes Hızı : " + mercedes.speed);
-        
+        Car s9000 = new Car("Corporate","Mercedes","Black");
+        s9000.printInfo();
     }
 }
--- Patika Java 101 Ders Notları/8_ Sınıflar - 6 Ders/OOP/src/Car.java
+++ Patika Java 101 Ders Notları/8_ Sınıflar - 6 Ders/OOP/src/Car.java
@@ -7,6 +7,21 @@ public class Car {
     String color;
     int speed;
 
+    // Constructors (Kurucu) Metot
+    Car(String type, String model, String color){
+        this.type = type;
+        this.model = model;
+        this.color = color;
+        this.speed = 0;
+        System.out.println("Constructor with parametres used...");
+        System.out.println("------");
+    }
+
+    Car(){
+        System.out.println("Default Empty Consturctor used....");
+        System.out.println("-----");
+    }
+
     // Davranışlar
     int increaseSpeed(int increment){
         speed += increment;
@@ -22,5 +37,13 @@ public class Car {
 
     void printSpeed(){
         System.out.println("Speed : " + speed);
+    }
+
+    void printInfo(){
+        System.out.println("================");
+        System.out.println("Model  : " + this.model);
+        System.out.println("Color  : " + this.color);
+        System.out.println("Type  : " + this.type);
+        System.out.println("Speed  : " + this.speed);
     }
 }
