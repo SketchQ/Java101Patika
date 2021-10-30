bf7e9beb727f64651185062d9001f7e2986fd5f8
Erim Serdönmez <eserdnmez@gmail.com>
2021-10-31 00:58:32 +0300

    Boxing Game Draft Finished

--- Patika Java 101 Ders Notları/8_ Sınıflar - 6 Ders/OOP/Boks/Main.java
+++ Patika Java 101 Ders Notları/8_ Sınıflar - 6 Ders/OOP/Boks/Main.java
@@ -2,6 +2,9 @@ package Boks;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Fighter marc = new Fighter("Mark", 15, 100, 90, 0);
+        Fighter alex = new Fighter("Alex", 20, 100, 95, 0);
+        Ring r = new Ring(marc, alex, 75, 120);
+        r.run();
     }
 }
--- Patika Java 101 Ders Notları/8_ Sınıflar - 6 Ders/OOP/Boks/Ring.java
+++ Patika Java 101 Ders Notları/8_ Sınıflar - 6 Ders/OOP/Boks/Ring.java
@@ -1,5 +1,56 @@
 package Boks;
 
 public class Ring {
-    
+    Fighter f1;
+    Fighter f2;
+    int minWeight;
+    int maxWeight;
+
+    public Ring(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+    }
+
+    public void run() {
+        if (checkweight()) {
+            while (f1.health > 0 && f2.health > 0) {
+                System.out.println("=========== New Round!! ==============");
+                f2.health = f1.hit(f2);
+                if (isWin()) {
+                    break;
+                }
+                f1.health = f2.hit(f1);
+                if (isWin()) {
+                    break;
+                }
+                printScore();
+            }
+        } else {
+            System.out.println("Sporcuların ağırlıkları uyuşmuyor!");
+        }
+    }
+
+    public boolean checkweight() {
+        return ((f1.weight >= this.minWeight && f1.weight <= this.maxWeight) && (f2.weight >= this.minWeight && f2.weight <= this.maxWeight));
+    }
+
+    public boolean isWin() {
+        if (f1.health == 0) {
+            System.out.println("Maçın kazananı \t: " + f2.name);
+            return true;
+        } else if (f2.health == 0) {
+            System.out.println("Maçın Kazananı \t: " + f1.name);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void printScore() {
+        System.out.println("=============");
+        System.out.println(f1.name + " Kalan Can \t: " + f1.health);
+        System.out.println(f2.name + " Kalan Can \t: " + f2.health);
+    }
 }
