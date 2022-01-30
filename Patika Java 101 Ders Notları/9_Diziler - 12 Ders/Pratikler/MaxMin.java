be17ffeffa6fff54fe6b32da0ca344b373ac29b1
Erim Serdönmez <eserdnmez@gmail.com>
2022-01-30 13:54:36 +0300

    initial Commit

---
+++ Patika Java 101 Ders Notları/9_Diziler - 12 Ders/Pratikler/MaxMin.java
@@ -0,0 +1,13 @@
+package Pratikler;
+
+// Dizideki Maksimum ve Minimum değerleri bulan program
+
+/*
+ *   @author Erim Serdönmez
+ */
+
+public class MaxMin {
+   public static void main(String[] args) {
+       
+   } 
+}
