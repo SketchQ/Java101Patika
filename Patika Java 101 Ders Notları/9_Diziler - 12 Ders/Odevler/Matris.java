d4574ec5ae8a1b5eb4444273f93854688f4d3c2a
Erim Serdönmez <eserdnmez@gmail.com>
2022-01-30 16:59:15 +0300

    initial Commit

---
+++ Patika Java 101 Ders Notları/9_Diziler - 12 Ders/Odevler/Matris.java
@@ -0,0 +1,13 @@
+package Odevler;
+
+// Java dilinde, çok boyutlu diziler ile oluşturulmuş matrisin transpozunu bulan programı yazınız.
+
+/*
+* @author: Erim Serdönmez
+*/
+
+public class Matris {
+    public static void main(String[] args) {
+
+    }
+}
