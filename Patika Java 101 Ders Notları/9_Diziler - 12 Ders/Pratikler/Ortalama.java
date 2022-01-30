e1d880b75cde56d6e70dfe52543011b08d21a391
Erim Serdönmez <eserdnmez@gmail.com>
2022-01-30 13:51:50 +0300

    Harmonik Ortalama

--- Patika Java 101 Ders Notları/9_Diziler - 12 Ders/Pratikler/Ortalama.java
+++ Patika Java 101 Ders Notları/9_Diziler - 12 Ders/Pratikler/Ortalama.java
@@ -5,9 +5,29 @@ package Pratikler;
 /*
 *   @author Erim Serdönmez
 */
-
-import java.util.Arrays;
-
 public class Ortalama {
+    public static void main(String[] args) {
+        int[] list = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        double sum = 0;
+
+        for (int i : list) {
+            sum += i;
+        }
+
+        double average = sum / list.length;
+        System.out.println("Ortalama: " + average);
+
+        // Harmonik ortalamasını yazan program
+        double harmonicSum = 0;
+
+        for (int i : list) {
+            harmonicSum += 1 / (double) i;
+        }
+
+        double harmonicAverage = list.length / harmonicSum;
+
+        System.out.println("Harmonik Ortalama: " + harmonicAverage);
+
+    }
     
 }
