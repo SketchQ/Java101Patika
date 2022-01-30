fbb2e5969e213e805bc1792cf0080414e2607730
Erim Serdönmez <eserdnmez@gmail.com>
2022-01-30 15:43:39 +0300

    Duplicate Numbers

--- Patika Java 101 Ders Notları/9_Diziler - 12 Ders/Pratikler/Tekrar.java
+++ Patika Java 101 Ders Notları/9_Diziler - 12 Ders/Pratikler/Tekrar.java
@@ -1,5 +1,6 @@
 package Pratikler;
 
+
 // Dizideki Tekrar eden Sayıları Bulan Java Programı
 
 /* 
@@ -7,7 +8,60 @@ package Pratikler;
 */
 
 public class Tekrar {
+    static boolean isFind(int[] arr, int val) {
+        for (int i : arr) {
+            if (i == val)
+                return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        
+        int[] dizi = { 3, 7, 3, 3, 2, 9, 10, 21, 1, 33, 9, 1 };
+        int[] tekrar = new int[dizi.length];
+        int startIndex = 0;
+
+        for (int i = 0; i < dizi.length; i++) {
+            for (int j = 0; j < dizi.length; j++) {
+                if (i != j && dizi[i] == dizi[j]) {
+                    if (!isFind(tekrar, dizi[i])) {
+                        tekrar[startIndex++] = dizi[i];
+                    }
+                    break;
+
+                }
+            }
+        }
+
+        for (int i : tekrar) {
+            if (i != 0)
+                System.out.print(i + " ");
+        }
+
+        System.out.println("\n------------------");
+
+        // Ödev -- Sayı dizisindeki tekrar eden çift sayılaı belirten bir program yazınız.
+        int[] array = {6, 40, 5, 20, 6, 8, 45, 20, 40, 5, 8, 45};
+        int[] duplicate = new int[array.length];
+        int start = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++){
+                if ( (i != j && array[i] == array[j]) && array[i] %2 == 0 ) {
+                    if (!isFind(duplicate, array[i])) {
+                        duplicate[start++] = array[i];
+                    }
+                    break;
+                }
+            }
+        }
+
+
+
+        for (int i : duplicate) {
+            if (i != 0)
+                System.out.print(i + " ");
+        }
+
     }
 }
