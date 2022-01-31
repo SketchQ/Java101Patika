a0aea871ae627d214bbd75a1a195e96827f298f7
Erim Serdönmez <eserdnmez@gmail.com>
2022-01-31 09:53:55 +0300

    initial

---
+++ Patika Java 101 Ders Notları/10_Diğer Konular/Odev/src/MineSweeper.java
@@ -0,0 +1,21 @@
+/* 
+    Java program to play text-based Minesweeper game.
+    The game is played on a square board of size NxN.
+    The board is divided into NxN cells.
+    Each cell is either empty or contains a mine.
+    The player is initially positioned in the middle of the board.
+    The player can move around the board and uncover cells.
+    If a cell contains a mine, the player loses the game.
+    If a cell does not contain a mine, the number of mines in the neighboring cells is displayed.
+    The player wins the game if all the cells without mines are uncovered.
+    The player can uncover a cell only if it is not already uncovered.
+
+    @author: Erim Serdönmez
+*/
+
+
+public class MineSweeper {
+    public static void main(String[] args) {
+        
+    }
+}
