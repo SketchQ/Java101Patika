bb89be3904ea12dd21eb0f189f426ac8de6a7bc6
Erim Serdönmez <eserdnmez@gmail.com>
2022-01-31 09:37:19 +0300

    initial Commit

---
+++ Patika Java 101 Ders Notları/10_Diğer Konular/Palindrome.java
@@ -0,0 +1,20 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String input = "";
+        String reverse = "";
+        boolean isPalindrome = false;
+        while (!isPalindrome) {
+            System.out.print("Enter a word: ");
+            input = System.console().readLine();
+            for (int i = input.length() - 1; i >= 0; i--) {
+                reverse += input.charAt(i);
+            }
+            if (input.equalsIgnoreCase(reverse)) {
+                isPalindrome = true;
+            } else {
+                reverse = "";
+            }
+        }
+        System.out.println("The word " + input + " is a palindrome.");
+    }
+}
