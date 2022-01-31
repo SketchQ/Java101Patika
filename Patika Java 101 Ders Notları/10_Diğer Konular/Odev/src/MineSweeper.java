3613c4403eeef5fe80dd6737e57e5db6f2dc8ace
Erim Serdönmez <eserdnmez@gmail.com>
2022-01-31 12:58:16 +0300

    Edit

--- Patika Java 101 Ders Notları/10_Diğer Konular/Odev/src/MineSweeper.java
+++ Patika Java 101 Ders Notları/10_Diğer Konular/Odev/src/MineSweeper.java
@@ -1,21 +1,27 @@
 /* 
-    Java program to play text-based Minesweeper game.
-    The game is played on a square board of size NxN.
-    The board is divided into NxN cells.
-    Each cell is either empty or contains a mine.
-    The player is initially positioned in the middle of the board.
-    The player can move around the board and uncover cells.
-    If a cell contains a mine, the player loses the game.
-    If a cell does not contain a mine, the number of mines in the neighboring cells is displayed.
-    The player wins the game if all the cells without mines are uncovered.
-    The player can uncover a cell only if it is not already uncovered.
+    Oyun metin tabanlıdır.
+
+    Çift boyutlu diziler üzerinden oynanmalı ve MineSweeper sınıfı içerisinde tasarlanmalı.
+    
+    Matris boyutunu yani satır ve sütun sayısını kullanıcı belirlemeli.
+    
+    Diziye ait eleman sayısının çeyreği (elemanSayisi / 4) kadar rastgele mayın yerleştirilmeli. Örneğin dizi 4x3 boyutunda ise eleman sayısı (satırSayısı * sütunSayısı) formülü ile hesaplanmalı ve boyutu 12 olmalı. Bu durumda mayın sayısı 12 / 4 = 3 adet olmalıdır. (ipucu : bu mayınların konumlarını tutacak ikinci bir dizi oluşturabilirsiniz.)
+    
+    Kullanıcı matris üzerinden bir nokta seçmeli. Nokta seçimi için satır ve sütun değerlerini girmeli.
+    
+    Seçilen noktanın dizinin sınırları içerisinde olup olmadığını kontrol edilmeli ve koşul sağlanmazsa tekrar nokta istenmeli.
+    
+    Kullanıcının girdiği noktada mayın var ise oyunu kaybetmeli.
+    
+    Mayın yok ise, ilgili noktaya değen tüm konumlarına bakılmalı (sağı, solu, yukarısı, aşağısı, sol üst çapraz, sağ üst çapraz, sağ alt çapraz, sol alt çapraz) ve etrafındaki mayınların sayısının toplamı ilgili noktaya yazılmalı. Noktaya değen herhangi bir mayın yok ise "0" değeri atanmalı.
+    
+    Kullanıcı hiç bir mayına basmadan tüm noktaları seçebilirse oyunu kazanmalı.
 
     @author: Erim Serdönmez
 */
 
-
 public class MineSweeper {
     public static void main(String[] args) {
-        
+
     }
 }
